01b4f8d456d73a52b9530018947f2539c71b792f
petestapp <63821853+petestapp@users.noreply.github.com>
2021-10-08 15:12:42 -0500

    pool.js set up

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const pool = require('./modules/pool');
 // uses
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -12,3 +13,12 @@ app.listen(port, ()=>{
     console.log('server is up on:', port);
 })
 // routes
+app.get('/tasks', (req, res)=>{
+    const queryString = `SELECT * FROM tasks`;
+    pool.query(queryString).then((results)=>{
+        res.send(results.rows);
+    }).catch((err)=>{
+        console.log(err);
+        res.sendStatus(500);
+    })
+})
